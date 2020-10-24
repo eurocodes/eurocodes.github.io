7fabfe7c0ba0617e0555cbf6dd4d1f109bfd1a5b
eurocodes <emmanuelumeoke@gmail.com>
2020-10-24 06:11:31 +0100

    Added update

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,82 @@ const sr = ScrollReveal({
     reset: true
 });
 
+// TYPED EFFECT
+let TextType = function (el, toType, period) {
+    this.toType = toType;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 200;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+};
+TextType.prototype.tick = function () {
+    let i = this.loopNum % this.toType.length;
+    let fullTxt = this.toType[i]
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    let that = this;
+    let delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; };
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+window.onload = function () {
+    let elements = document.getElementsByClassName("typed");
+    for (let i = 0; i < elements.length; i++) {
+        let toType = elements[i].getAttribute("data-rotate");
+        let period = elements[i].getAttribute("data-period");
+
+        if (toType) {
+            new TextType(elements[i], JSON.parse(toType), period);
+        }
+    }
+    // Inject css
+    let css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typed > .wrap {border-right: 0.08em solid #307ccc}";
+    document.body.appendChild(css);
+};
+
+// PROJECT SECTION
+let projects = document.getElementById("project");
+let projectImage = projects.getElementsByClassName("image");
+let descText = projects.getElementsByClassName("light-text");
+
+for (let i = 0; i < projectImage.length; i++) {
+    projectImage[i].addEventListener("mouseover", () => {
+        descText[i].classList.add("shown");
+        descText[i].classList.remove("hidden");
+    })
+}
+
+for (let i = 0; i < projectImage.length; i++) {
+    projectImage[i].addEventListener("mouseout", () => {
+        descText[i].classList.remove("shown");
+        descText[i].classList.add("hidden");
+    })
+}
+
 /*SCROLL HOME*/
 sr.reveal('.home__title', {});
 sr.reveal('.button', { delay: 200 });
